c18af98ade0927d25c74e698b6adf37d3f15a153
Frances Thai <notfelineit@gmail.com>
2020-08-27 15:38:16 -0700

    Updates

--- labor/service-worker.js
+++ labor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://getstarted.nj.gov/labor/precache-manifest.0a237d8808b272c550902f78203cad08.js"
+  "https://getstarted.nj.gov/labor/precache-manifest.f986a05dd2b2c29be8730d277d32ef3c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://getstarted.nj.gov/labor/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
